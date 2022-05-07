9763202bf9fcf32be4ba2ec8b0e08c81873b822e
git-jslee <datadam@kakao.com>
2022-05-07 19:08:31 +0900

    220507

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+
+    }
+
+    public void ProcessInput(Vector3 moveDirection)
+    {
+
+    }
+}
